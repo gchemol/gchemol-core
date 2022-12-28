6a91ef1214c31ec0519aaddb1317b65919294d74
Wenping Guo <ybyygu@gmail.com>
2022-12-28 15:49:51 +0800

    Molecule: deprecate distance method; add methods for angle, torsion

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,10 +1,9 @@
-// imports
-
+// [[file:../gchemol-core.note::*imports][imports:1]]
 use crate::{Atom, Molecule};
 use crate::{Point3, Vector3f};
+// imports:1 ends here
 
-// api/core
-
+// [[file:../gchemol-core.note::*api/core][api/core:1]]
 /// Geometry related methods
 impl Molecule {
     /// Translate the whole molecule by a displacement
@@ -49,8 +48,10 @@ impl Molecule {
         }
     }
 }
+// api/core:1 ends here
 
-// api/distance
+// [[file:../gchemol-core.note::95de44db][95de44db]]
+use gchemol_geometry::prelude::*;
 
 impl Atom {
     /// Return distance to other atom.
@@ -69,20 +70,44 @@ impl Molecule {
     /// # Panic
     ///
     /// * if atom indices `i` or `j` out of range.
+    #[deprecated(note = "get_distance instead")]
     pub fn distance(&self, i: usize, j: usize) -> f64 {
-        match (self.get_atom(i), self.get_atom(j)) {
-            (Some(ai), Some(aj)) => {
-                if let Some(mut lat) = self.lattice {
-                    let pi = ai.position();
-                    let pj = aj.position();
-                    lat.distance(pi, pj)
-                } else {
-                    ai.distance(aj)
-                }
-            }
-            _ => {
-                panic!("atom indices out of range: {}, {}", i, j);
-            }
+        self.get_distance(i, j).expect("invalid serial numbers")
+    }
+
+    /// Return the distance of two atoms `i`, `j`. For periodic
+    /// structure, this method will return the distance under the
+    /// minimum image convention. Return None if any serial number is
+    /// invalid.
+    pub fn get_distance(&self, i: usize, j: usize) -> Option<f64> {
+        let pi = self.get_atom(i)?.position();
+        let pj = self.get_atom(j)?.position();
+        if let Some(lat) = self.lattice {
+            lat.distance(pi, pj).into()
+        } else {
+            pi.distance(pj).into()
         }
     }
+
+    /// Return the angle of three atoms `i`, `j`, `k` in radians,
+    /// irrespective periodic images. Return None if any serial number
+    /// is invalid.
+    pub fn get_angle(&self, i: usize, j: usize, k: usize) -> Option<f64> {
+        let pi = self.get_atom(i)?.position();
+        let pj = self.get_atom(j)?.position();
+        let pk = self.get_atom(k)?.position();
+        pi.angle(pj, pk).into()
+    }
+
+    /// Return the torsion angle of four atoms `i`, `j`, `k`, `l` in
+    /// radians, irrespective periodic images. Return None if any serial
+    /// number is invalid.
+    pub fn get_torsion(&self, i: usize, j: usize, k: usize, l: usize) -> Option<f64> {
+        let pi = self.get_atom(i)?.position();
+        let pj = self.get_atom(j)?.position();
+        let pk = self.get_atom(k)?.position();
+        let pl = self.get_atom(l)?.position();
+        pi.torsion(pj, pk, pl).into()
+    }
 }
+// 95de44db ends here
