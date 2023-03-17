9607cceb0a5ecaaf698b5facb337590f9eb76e75
Wenping Guo <ybyygu@gmail.com>
2023-03-17 13:46:23 +0800

    mol: return formula according to the Hill system order

--- src/formula.rs
+++ src/formula.rs
@@ -1,26 +1,25 @@
-// imports
+// [[file:../gchemol-core.note::739e8c94][739e8c94]]
+//! Calculate the molecular formula according to the Hill system order.
+//!
+//! https://web.stanford.edu/group/swain/cinf/workshop98aug/hill.html
+// 739e8c94 ends here
 
-// [[file:~/Workspace/Programming/gchemol-rs/gchemol-core/gchemol-core.note::*imports][imports:1]]
+// [[file:../gchemol-core.note::*imports][imports:1]]
 use crate::molecule::Molecule;
 
 use std::collections::HashMap;
 use std::iter::IntoIterator;
 // imports:1 ends here
 
-// core
-
-// [[file:~/Workspace/Programming/gchemol-rs/gchemol-core/gchemol-core.note::*core][core:1]]
+// [[file:../gchemol-core.note::0d8318bb][0d8318bb]]
 fn get_reduced_symbols<'a, I>(symbols: I) -> HashMap<String, usize>
 where
     I: IntoIterator,
     I::Item: std::fmt::Display,
 {
-    let symbols: Vec<_> = symbols
-        .into_iter()
-        .map(|item| format!("{:}", item))
-        .collect();
+    let symbols: Vec<_> = symbols.into_iter().map(|item| format!("{:}", item)).collect();
 
-    // 1. count symbols: CCCC ==> C 4
+    // count symbols: CCCC ==> C 4
     let mut counts = HashMap::new();
     for x in symbols {
         let c = counts.entry(x).or_insert(0);
@@ -35,31 +34,30 @@ where
     I: IntoIterator,
     I::Item: std::fmt::Display,
 {
-    let counts = get_reduced_symbols(symbols);
+    let sym_and_counts = get_reduced_symbols(symbols);
+    let mut syms: Vec<_> = sym_and_counts.keys().collect();
+    syms.sort_by_key(|k| match k.as_str() {
+        // Carbon first, Hydrogen second, and all remaining elements,
+        // including Deuterium and Tritium, in alphabetical order.
+        "C" => "0",
+        "H" => "1",
+        // If no Carbon is present, put all elements in alphabetical order.
+        _ => k,
+    });
 
-    let mut syms: Vec<String> = Vec::new();
-    let mut to_append = String::new();
-    // 2. format the formula
-    for (k, v) in counts {
-        // 2.1 omit number if the count is 1: C1H4 ==> CH4
-        let mut s = String::new();
-        if v > 1 {
-            s = v.to_string();
-        }
-        // 2.2 special treatments for C and H
-        let reduced = format!("{}{}", k, s);
-        if k == "C" {
-            syms.insert(0, reduced);
-        } else if k == "H" {
-            to_append = reduced;
-        } else {
-            syms.push(reduced);
-        }
-    }
-    // 3. final output
-    syms.push(to_append);
-    let formula = syms.join("");
-    formula
+    let s: Vec<_> = syms
+        .into_iter()
+        .map(|k| {
+            let n = sym_and_counts[k];
+            // omit number if the count is 1: C1H4 ==> CH4
+            if n == 1 {
+                format!("{k}")
+            } else {
+                format!("{k}{n}")
+            }
+        })
+        .collect();
+    s.join("")
 }
 
 #[test]
@@ -69,17 +67,20 @@ fn test_formula() {
     assert_eq!("C2H4", formula);
     let symbols = vec!["C", "H", "C", "H", "H", "O", "H", "O"];
     let formula = get_reduced_formula(&symbols);
-    assert_eq!("C2O2H4", formula);
+    assert_eq!("C2H4O2", formula);
+
+    let symbols = vec!["H", "H", "H", "O", "H", "O"];
+    let formula = get_reduced_formula(&symbols);
+    assert_eq!("H4O2", formula);
 }
-// core:1 ends here
+// 0d8318bb ends here
 
-// api
-
-// [[file:~/Workspace/Programming/gchemol-rs/gchemol-core/gchemol-core.note::*api][api:1]]
+// [[file:../gchemol-core.note::3c178411][3c178411]]
 /// Chemical formula
 impl Molecule {
-    /// Return the molecule formula represented in string
-    /// Return empty string if molecule containing no atom
+    /// Return the molecule formula represented in string according to
+    /// the Hill system order. Return empty string if molecule
+    /// containing no atom.
     pub fn formula(&self) -> String {
         get_reduced_formula(self.symbols())
     }
@@ -89,4 +90,4 @@ impl Molecule {
         get_reduced_symbols(self.symbols())
     }
 }
-// api:1 ends here
+// 3c178411 ends here
